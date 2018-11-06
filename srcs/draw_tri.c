3c7687aa5e488dd7e8811b607f62cc3e0a34539a
notoriousgtw <notoriousgtw@gmail.com>
2018-11-06 14:21:20 -0800

    draws cubes!

--- srcs/draw_tri.c
+++ srcs/draw_tri.c
@@ -6,9 +6,156 @@
 /*   By: gwood <gwood@42.us.org>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 23:39:53 by gwood             #+#    #+#             */
-/*   Updated: 2018/11/05 21:02:59 by gwood            ###   ########.fr       */
+/*   Updated: 2018/11/06 13:36:00 by gwood            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/triangle.h"
 #include <stdio.h>
+
+// static void	kt_draw_tri_scanline(t_xvars *x_vars, t_drawtri *dt, int y, int color)
+// {
+// 	int		x;
+// 	int		x_end;
+// 	double	dx;
+// 	double	z;
+// 	t_vert	tmp;
+
+// 	x = (int) ceil(dt->it_edge0.pos.y - 0.5);
+// 	x_end = (int) ceil(dt->it_edge1.pos.y - 0.5);
+// 	kt_vert_dup(&dt->it_edge0, &dt->i_line);
+// 	dx = dt->it_edge1.pos.x - dt->it_edge0.pos.x;
+// 	kt_vert_sub(&dt->it_edge1, &dt->i_line, &tmp);
+// 	kt_vert_div(&tmp, dx, &dt->di_line);
+// 	kt_vert_mult(&dt->di_line, ((double) x + 0.5 - dt->it_edge0.pos.x), &tmp);
+// 	kt_vert_add(&tmp, &dt->i_line, &dt->i_line);
+// 	while (x < x_end)
+// 	{
+// 		z = 1.0 / dt->i_line.pos.z;
+// 		kt_vert_dup(&dt->i_line, &tmp);
+// 		kt_vert_mult(&tmp, z, &tmp);
+// 		XSetForeground(x_vars->dpy, x_vars->gc, color);
+// 		XDrawPoint(x_vars->dpy, x_vars->win, x_vars->gc, x, y);
+// 		kt_vert_add(&dt->i_line, &dt->di_line, &dt->i_line);
+// 		x++;
+// 	}
+// }
+
+// static void	kt_draw_flat_tri(t_xvars *x_vars, t_drawtri *dt, int color)
+// {
+// 	int		y;
+// 	int		y_end;
+// 	t_vert	tmp;
+
+// 	kt_vert_dup(dt->it0, &dt->it_edge0);
+// 	kt_vert_dup(dt->it0, &tmp);
+// 	y = (int) ceil(dt->it0->pos.y - 0.5);
+// 	y_end = (int) ceil(dt->it2->pos.y - 0.5);
+// 	kt_vert_mult(&dt->dit0, ((double) y + 0.5 - dt->it0->pos.y), &tmp);
+// 	kt_vert_add(&dt->it_edge0, &tmp, &dt->it_edge0);
+// 	kt_vert_mult(&dt->dit1, ((double) y + 0.5 - dt->it0->pos.y), &tmp);
+// 	kt_vert_add(&dt->it_edge1, &tmp, &dt->it_edge1);
+// 	while (y < y_end)
+// 	{
+// 		kt_draw_tri_scanline(x_vars, dt, y, color);
+// 		kt_vert_add(&dt->it_edge0, &dt->dit0, &dt->it_edge0);
+// 		kt_vert_add(&dt->it_edge1, &dt->dit1, &dt->it_edge1);
+// 		y++;
+// 	}
+
+// }
+
+// static void	kt_draw_flat_tri_top(t_xvars *x_vars, t_drawtri *dt, int color)
+// {
+// 	double	dy;
+
+// 	dy = dt->it2->pos.y - dt->it0->pos.y;
+// 	kt_vert_dup(dt->it2, &dt->dit0);
+// 	kt_vert_sub(&dt->dit0, dt->it0, &dt->dit0);
+// 	kt_vert_div(&dt->dit0, dy, &dt->dit0);
+// 	kt_vert_dup(dt->it2, &dt->dit1);
+// 	kt_vert_sub(&dt->dit1, dt->it1, &dt->dit1);
+// 	kt_vert_div(&dt->dit1, dy, &dt->dit1);
+// 	kt_vert_dup(dt->it1, &dt->it_edge1);
+// 	kt_draw_flat_tri(x_vars, dt, color);
+// }
+
+// static void	kt_draw_flat_tri_bottom(t_xvars *x_vars, t_drawtri *dt, int color)
+// {
+// 	double	dy;
+
+// 	dy = dt->it2->pos.y - dt->it0->pos.y;
+// 	kt_vert_dup(dt->it1, &dt->dit0);
+// 	kt_vert_sub(&dt->dit0, dt->it0, &dt->dit0);
+// 	kt_vert_div(&dt->dit0, dy, &dt->dit0);
+// 	kt_vert_dup(dt->it2, &dt->dit1);
+// 	kt_vert_sub(&dt->dit1, dt->it0, &dt->dit1);
+// 	kt_vert_div(&dt->dit1, dy, &dt->dit1);
+// 	kt_vert_dup(dt->it0, &dt->it_edge1);
+// 	kt_draw_flat_tri(x_vars, dt, color);
+// }
+
+// void		kt_tri_draw(t_xvars *x_vars, t_tri *tri, int color)
+// {
+// 	t_drawtri	dt;
+// 	double		alpha_split;
+// 	t_vert		vi;
+
+// 	dt.it0 = &tri->v0;
+// 	dt.it1 = &tri->v1;
+// 	dt.it2 = &tri->v2;
+// 	if (tri->v1.pos.y < tri->v0.pos.y)
+// 		kt_vec3d_swap(&tri->v0.pos, &tri->v1.pos);
+// 	if (tri->v2.pos.y < tri->v1.pos.y)
+// 		kt_vec3d_swap(&tri->v1.pos, &tri->v2.pos);
+// 	if (tri->v1.pos.y < tri->v0.pos.y)
+// 		kt_vec3d_swap(&tri->v0.pos, &tri->v1.pos);
+// 	if (tri->v0.pos.y == tri->v1.pos.y)
+// 	{
+// 		if (tri->v1.pos.x < tri->v0.pos.x)
+// 			kt_vec3d_swap(&tri->v0.pos, &tri->v1.pos);
+// 		kt_tri_print(tri);
+// 		printf("\n");
+// 		kt_draw_flat_tri_top(x_vars, &dt, color);
+// 	}
+// 	else if (tri->v1.pos.y == tri->v2.pos.y)
+// 	{
+// 		if (tri->v2.pos.x < tri->v1.pos.x)
+// 			kt_vec3d_swap(&tri->v1.pos, &tri->v2.pos);
+// 		kt_tri_print(tri);
+// 		printf("\n");
+// 		kt_draw_flat_tri_bottom(x_vars, &dt, color);
+// 	}
+// 	else
+// 	{
+// 		kt_tri_print(tri);
+// 		printf("\n");
+// 		alpha_split = (tri->v1.pos.y - tri->v0.pos.y) / (tri->v2.pos.y - tri->v0.pos.y);
+// 		kt_vert_dup(&tri->v0, &vi);
+// 		kt_vert_sub(&tri->v2, &tri->v0, &vi);
+// 		kt_vert_mult(&vi, alpha_split, &vi);
+// 		kt_vert_add(&tri->v0, &vi, &vi);
+// 		if (tri->v1.pos.x < vi.pos.x)
+// 		{
+// 			dt.it0 = &tri->v0;
+// 			dt.it1 = &tri->v1;
+// 			dt.it2 = &vi;
+// 			kt_draw_flat_tri_bottom(x_vars, &dt, color);
+// 			dt.it0 = &tri->v0;
+// 			dt.it1 = &vi;
+// 			dt.it2 = &tri->v2;
+// 			kt_draw_flat_tri_top(x_vars, &dt, color);
+// 		}
+// 		else
+// 		{
+// 			dt.it0 = &tri->v0;
+// 			dt.it1 = &vi;
+// 			dt.it2 = &tri->v2;
+// 			// kt_draw_flat_tri_bottom(x_vars, &dt, color);
+// 			dt.it0 = &vi;
+// 			dt.it1 = &tri->v1;
+// 			dt.it2 = &tri->v2;
+// 			kt_draw_flat_tri_top(x_vars, &dt, color);
+// 		}
+// 	}
+// }
--- srcs/pipeline_draw.c
+++ srcs/pipeline_draw.c
@@ -6,7 +6,7 @@
 /*   By: gwood <gwood@42.us.org>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 23:39:53 by gwood             #+#    #+#             */
-/*   Updated: 2018/11/05 22:38:50 by gwood            ###   ########.fr       */
+/*   Updated: 2018/11/06 14:20:20 by gwood            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ static void	kt_pipeline_draw_tri_scanline(t_pipeline *p, t_drawtri *dt, int y)
 	double	z;
 	t_vert	tmp;
 
-	x = (int) ceil(dt->it_edge0.pos.y - 0.5);
-	x_end = (int) ceil(dt->it_edge1.pos.y - 0.5);
+	x = (int) ceil(dt->it_edge0.pos.x - 0.5);
+	x_end = (int) ceil(dt->it_edge1.pos.x - 0.5);
 	kt_vert_dup(&dt->it_edge0, &dt->i_line);
 	dx = dt->it_edge1.pos.x - dt->it_edge0.pos.x;
 	kt_vert_sub(&dt->it_edge1, &dt->i_line, &tmp);
@@ -66,51 +66,57 @@ static void	kt_pipeline_draw_flat_tri(t_pipeline *p, t_drawtri *dt)
 	}
 }
 
-static void	kt_pipeline_draw_flat_tri_top(t_pipeline *p, t_drawtri *dt)
+static void	kt_pipeline_draw_flat_tri_top(t_pipeline *p, const t_vert *it0,
+											 const t_vert *it1, const t_vert *it2)
 {
-	double	dy;
+	double		dy;
+	t_drawtri	dt;
 
-	kt_drawline3d(p->x, dt->it0.pos, dt->it1.pos, GREEN);
-	kt_drawline3d(p->x, dt->it1.pos, dt->it2.pos, GREEN);
-	kt_drawline3d(p->x, dt->it2.pos, dt->it0.pos, GREEN);
-	dy = dt->it2.pos.y - dt->it0.pos.y;
-	kt_vert_dup(&dt->it2, &dt->dit0);
-	kt_vert_sub(&dt->it2, &dt->it0, &dt->dit0);
-	kt_vert_div(&dt->dit0, dy, &dt->dit0);
-	kt_vert_dup(&dt->it2, &dt->dit1);
-	kt_vert_sub(&dt->it2, &dt->it1, &dt->dit1);
-	kt_vert_div(&dt->dit1, dy, &dt->dit1);
-	kt_vert_dup(&dt->it1, &dt->it_edge1);
-	kt_pipeline_draw_flat_tri(p, dt);
+	kt_vert_dup(it0, &dt.it0);
+	kt_vert_dup(it1, &dt.it1);
+	kt_vert_dup(it2, &dt.it2);
+	// kt_drawline3d(p->x, dt.it0.pos, dt.it1.pos, GREEN);
+	// kt_drawline3d(p->x, dt.it1.pos, dt.it2.pos, GREEN);
+	// kt_drawline3d(p->x, dt.it2.pos, dt.it0.pos, GREEN);
+	dy = dt.it2.pos.y - dt.it0.pos.y;
+	kt_vert_dup(&dt.it2, &dt.dit0);
+	kt_vert_sub(&dt.it2, &dt.it0, &dt.dit0);
+	kt_vert_div(&dt.dit0, dy, &dt.dit0);
+	kt_vert_dup(&dt.it2, &dt.dit1);
+	kt_vert_sub(&dt.it2, &dt.it1, &dt.dit1);
+	kt_vert_div(&dt.dit1, dy, &dt.dit1);
+	kt_vert_dup(&dt.it1, &dt.it_edge1);
+	kt_pipeline_draw_flat_tri(p, &dt);
 }
 
-static void	kt_pipeline_draw_flat_tri_bottom(t_pipeline *p, t_drawtri *dt)
+static void	kt_pipeline_draw_flat_tri_bottom(t_pipeline *p, const t_vert *it0,
+											 const t_vert *it1, const t_vert *it2)
 {
-	double	dy;
+	double		dy;
+	t_drawtri	dt;
 
-	kt_drawline3d(p->x, dt->it0.pos, dt->it1.pos, RED);
-	kt_drawline3d(p->x, dt->it1.pos, dt->it2.pos, RED);
-	kt_drawline3d(p->x, dt->it2.pos, dt->it0.pos, RED);
-	dy = dt->it2.pos.y - dt->it0.pos.y;
-	kt_vert_dup(&dt->it1, &dt->dit0);
-	kt_vert_sub(&dt->dit0, &dt->it0, &dt->dit0);
-	kt_vert_div(&dt->dit0, dy, &dt->dit0);
-	kt_vert_dup(&dt->it2, &dt->dit1);
-	kt_vert_sub(&dt->dit1, &dt->it0, &dt->dit1);
-	kt_vert_div(&dt->dit1, dy, &dt->dit1);
-	kt_vert_dup(&dt->it0, &dt->it_edge1);
-	kt_pipeline_draw_flat_tri(p, dt);
+	kt_vert_dup(it0, &dt.it0);
+	kt_vert_dup(it1, &dt.it1);
+	kt_vert_dup(it2, &dt.it2);
+	// kt_drawline3d(p->x, dt.it0.pos, dt.it1.pos, RED);
+	// kt_drawline3d(p->x, dt.it1.pos, dt.it2.pos, GREEN);
+	// kt_drawline3d(p->x, dt.it2.pos, dt.it0.pos, BLUE);
+	dy = dt.it2.pos.y - dt.it0.pos.y;
+	kt_vert_dup(&dt.it1, &dt.dit0);
+	kt_vert_sub(&dt.it1, &dt.it0, &dt.dit0);
+	kt_vert_div(&dt.dit0, dy, &dt.dit0);
+	kt_vert_dup(&dt.it2, &dt.dit1);
+	kt_vert_sub(&dt.it2, &dt.it0, &dt.dit1);
+	kt_vert_div(&dt.dit1, dy, &dt.dit1);
+	kt_vert_dup(&dt.it0, &dt.it_edge1);
+	kt_pipeline_draw_flat_tri(p, &dt);
 }
 
 void			kt_pipeline_draw_tri(t_pipeline *p, t_tri *tri)
 {
-	t_drawtri	dt;
 	double		alpha_split;
 	t_vert		vi;
 
-	kt_vert_dup(&tri->v0, &dt.it0);
-	kt_vert_dup(&tri->v1, &dt.it1);
-	kt_vert_dup(&tri->v2, &dt.it2);
 	if (tri->v1.pos.y < tri->v0.pos.y)
 		kt_vec3d_swap(&tri->v0.pos, &tri->v1.pos);
 	if (tri->v2.pos.y < tri->v1.pos.y)
@@ -121,13 +127,13 @@ void			kt_pipeline_draw_tri(t_pipeline *p, t_tri *tri)
 	{
 		if (tri->v1.pos.x < tri->v0.pos.x)
 			kt_vec3d_swap(&tri->v0.pos, &tri->v1.pos);
-		kt_pipeline_draw_flat_tri_top(p, &dt);
+		kt_pipeline_draw_flat_tri_top(p, &tri->v0, &tri->v1, &tri->v2);
 	}
 	else if (tri->v1.pos.y == tri->v2.pos.y)
 	{
 		if (tri->v2.pos.x < tri->v1.pos.x)
 			kt_vec3d_swap(&tri->v1.pos, &tri->v2.pos);
-		kt_pipeline_draw_flat_tri_bottom(p, &dt);
+		kt_pipeline_draw_flat_tri_bottom(p, &tri->v0, &tri->v1, &tri->v2);
 	}
 	else
 	{
@@ -138,25 +144,13 @@ void			kt_pipeline_draw_tri(t_pipeline *p, t_tri *tri)
 		kt_vert_add(&tri->v0, &vi, &vi);
 		if (tri->v1.pos.x < vi.pos.x)
 		{
-			kt_vert_dup(&tri->v0, &dt.it0);
-			kt_vert_dup(&tri->v1, &dt.it1);
-			kt_vert_dup(&vi, &dt.it2);
-			kt_pipeline_draw_flat_tri_bottom(p, &dt);
-			kt_vert_dup(&tri->v1, &dt.it0);
-			kt_vert_dup(&vi, &dt.it1);
-			kt_vert_dup(&tri->v2, &dt.it2);
-			kt_pipeline_draw_flat_tri_top(p, &dt);
+			kt_pipeline_draw_flat_tri_bottom(p, &tri->v0, &tri->v1, &vi);
+			kt_pipeline_draw_flat_tri_top(p, &tri->v1, &vi, &tri->v2);
 		}
 		else
 		{
-			kt_vert_dup(&tri->v0, &dt.it0);
-			kt_vert_dup(&vi, &dt.it1);
-			kt_vert_dup(&tri->v1, &dt.it2);
-			kt_pipeline_draw_flat_tri_bottom(p, &dt);
-			kt_vert_dup(&vi, &dt.it0);
-			kt_vert_dup(&tri->v1, &dt.it1);
-			kt_vert_dup(&tri->v2, &dt.it2);
-			kt_pipeline_draw_flat_tri_top(p, &dt);
+			kt_pipeline_draw_flat_tri_bottom(p, &tri->v0, &vi, &tri->v1);
+			kt_pipeline_draw_flat_tri_top(p, &vi, &tri->v1, &tri->v2);
 		}
 	}
 }
